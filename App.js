822585c1b8da58851ab4f8c69fd9b872af6f9dd1
Danil Khrustov <khrustovd@gmail.com>
2019-07-20 16:01:36 -0500

    resolving TS vs JS issues in app main

--- App.js
+++ App.js
@@ -14,7 +14,6 @@ export default function App(props) {
     return (
       <AppLoading
         startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
         onFinish={() => handleFinishLoading(setLoadingComplete)}
       />
     );
@@ -35,19 +34,12 @@ async function loadResourcesAsync() {
       require('./assets/images/robot-prod.png'),
     ]),
     Font.loadAsync({
-      // This is the font that we are using for our tab bar
+
       ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
+
       'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
     }),
   ]);
-}
-
-function handleLoadingError(error: Error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
 }
 
 function handleFinishLoading(setLoadingComplete) {
