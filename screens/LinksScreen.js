f6cc6af22981520ca8568434b8028a6fab714178
Danil Khrustov <khrustovd@gmail.com>
2019-07-26 12:56:20 -0500

    refactor

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -6,14 +6,6 @@ export default function LinksScreen() {
   return (
     <ScrollView style={styles.container}>
       <Text>This is sample text for now</Text>
-      <Text>This is sample text for now</Text>
-      <Text>This is sample text for now</Text>
-      <Text>This is sample text for now</Text>
-      <Text>This is sample text for now</Text>
-      <Text>This is sample text for now</Text>
-      <Text>This is sample text for now</Text>
-      <Text>This is sample text for now</Text>
-      <Text>This is sample text for now</Text>
     </ScrollView>
   );
 }
